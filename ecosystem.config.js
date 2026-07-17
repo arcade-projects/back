10f0e8a4c87ae530df7c7e163f305146501ed010
mahankabir <mahan.kabir@gmail.com>
2026-07-17 21:34:21 +0300

    feat(ecosystem): add ecosystem file.

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'board_back',
+      script: 'dist/main.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 9080,
+      },
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      log_merge: true,
+      time: true
+    },
+  ],
+};
