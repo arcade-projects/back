fa3c2f2fe915dc6e7b71133a87c4b1f74f90e49d
mahankabir <mahan.kabir@gmail.com>
2026-07-31 16:51:29 +0300

    fix(ecosystem): update ecosystem.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,13 @@ module.exports = {
       exec_mode: 'fork',
       watch: false,
       max_memory_restart: '1G',
+      env_file: '.env',
       env: {
         NODE_ENV: 'production',
         PORT: 9090,
+        REDIS_HOST: '127.0.0.1',
+        REDIS_PORT: 6379,
+        REDIS_URL: 'redis://127.0.0.1:6379',
       },
       error_file: 'logs/err.log',
       out_file: 'logs/out.log',
